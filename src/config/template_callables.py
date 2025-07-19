043fbc8a2c69c24cb238ee6cf6d449369a632d28
Dmitriy Lunev <dima.lunev14@gmail.com>
2025-07-19 17:05:06 +0300

    Fix SEO + format html

--- src/config/template_callables.py
+++ src/config/template_callables.py
@@ -10,8 +10,16 @@ def get_static_file_url(ctx: Mapping[str, Any], file_path: str) -> str:  # noqa:
     return settings.get_minio_object_url(bucket="static", object_path=file_path)
 
 
+def get_full_url(ctx: Mapping[[str, Any]], path: str) -> str:  # noqa: ARG001
+    return settings.get_url(path=path)
+
+
 def register_template_callables(engine: JinjaTemplateEngine) -> None:
     engine.register_template_callable(
         key="get_static_file_url",
         template_callable=get_static_file_url,
+    )
+    engine.register_template_callable(
+        key="get_full_url",
+        template_callable=get_full_url,
     )
