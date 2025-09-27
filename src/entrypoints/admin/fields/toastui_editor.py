d952bfbe872ec164edcc68880ea4a641618decd1
Dmitriy Lunev <dima.lunev14@gmail.com>
2025-09-27 20:21:00 +0300

    Refactor ToastUIEditorWidget: add params parsing, simplify attrs get and update kwargs from field flags

--- src/entrypoints/admin/fields/toastui_editor.py
+++ src/entrypoints/admin/fields/toastui_editor.py
@@ -1,4 +1,6 @@
-from typing import Any
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Literal
 
 from jinja2 import BaseLoader, Environment
 from markupsafe import Markup
@@ -6,6 +8,19 @@ from wtforms.fields import TextAreaField
 from wtforms.widgets import html_params
 
 from config.constants import constants
+
+toastui_editor_template: Path = constants.path.template_dir / "sqladmin" / "_toastui_editor.html"
+
+
+@dataclass(kw_only=True)
+class ToastUIEditorRenderParams:
+    textarea_id: str
+    textarea_name: str
+    attrs: dict[str, Any]
+    class_: str
+    initial_value: str
+    edit_type: Literal["markdown", "wysiwyg"]
+    height: str
 
 
 class ToastUIEditorWidget:
@@ -17,25 +32,28 @@ class ToastUIEditorWidget:
         "minlength",
     }
 
-    def __call__(self, field: "ToastUIEditorField", **kwargs: Any) -> Markup:  # noqa: ANN401
-        flags = getattr(field, "flags", {})
-        for k in dir(flags):
-            if k in self.validation_attrs and k not in kwargs:
-                kwargs[k] = getattr(flags, k)
+    def prepare_params(
+        self,
+        field: "ToastUIEditorField",
+        kwargs: dict[str, Any],
+    ) -> ToastUIEditorRenderParams:
+        kwargs.update({k: v for k, v in vars(field.flags).items() if k in self.validation_attrs})
         attrs = {k: v for k, v in kwargs.items() if k in self.validation_attrs}
-        editor_template = constants.path.template_dir / "sqladmin" / "_toastui_editor.html"
+        return ToastUIEditorRenderParams(
+            textarea_id=field.id,
+            textarea_name=field.name,
+            attrs=html_params(**attrs),
+            class_=kwargs.pop("class", ""),
+            initial_value=field._value(),
+            edit_type="markdown",
+            height="auto",
+        )
+
+    def __call__(self, field: "ToastUIEditorField", **kwargs: Any) -> Markup:  # noqa: ANN401
         return Markup(  # noqa: S704
             Environment(loader=BaseLoader(), autoescape=True)
-            .from_string(editor_template.read_text())
-            .render(
-                textarea_id=field.id,
-                textarea_name=field.name,
-                attrs=html_params(**attrs),
-                class_=kwargs.get("class", ""),
-                initial_value=field._value(),  # noqa: SLF001
-                edit_type="markdown",
-                height="auto",
-            ),
+            .from_string(toastui_editor_template.read_text())
+            .render(params=self.prepare_params(field=field, kwargs=kwargs)),
         )
 
 
