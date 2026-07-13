f4c6e54bb044238c39935ab899bdaababcd69458
Dmitriy Lunev <dima.lunev14@gmail.com>
2026-07-13 15:18:17 +0300

    Backend(refactoring): HexUuidIDMixin now inherit BaseModelMixin and decorated by declarative_mixin.

--- backend/src/infra/postgresql/models/mixins/ids.py
+++ backend/src/infra/postgresql/models/mixins/ids.py
@@ -1,18 +1,27 @@
 import uuid
 
 from sqlalchemy import String, cast, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, synonym
+from sqlalchemy.orm.decl_api import declarative_mixin, declared_attr
+from sqlalchemy_dev_utils.mixins.base import BaseModelMixin
 
 
 def generate_uuid4_hex() -> str:
     return uuid.uuid4().hex
 
 
-class HexUuidIDMixin:
-    id: Mapped[str] = mapped_column(
-        String(length=32),
-        primary_key=True,
-        default=generate_uuid4_hex,
-        server_default=func.replace(cast(func.gen_random_uuid(), String()), "-", ""),
-        doc="UUIDv4 hex identifier",
-    )
+@declarative_mixin
+class HexUuidIDMixin(BaseModelMixin):
+    @declared_attr
+    def id(cls) -> Mapped[int]:
+        return mapped_column(
+            String(length=32),
+            primary_key=True,
+            default=generate_uuid4_hex,
+            server_default=func.replace(cast(func.gen_random_uuid(), String()), "-", ""),
+            doc="UUIDv4 hex identifier",
+        )
+
+    @declared_attr
+    def pk(cls) -> Mapped[int]:
+        return synonym("id")
