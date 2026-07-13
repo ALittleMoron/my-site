a5c662fa33370cb73a293046c7425786db81f823
Dmitriy Lunev <dima.lunev14@gmail.com>
2026-07-13 16:06:16 +0300

    Infra(fix): LightHouse now runs 3 times with median aggregation.

--- frontend/lighthouserc.cjs
+++ frontend/lighthouserc.cjs
@@ -6,7 +6,7 @@ module.exports = {
       startServerCommand: 'node scripts/lhci_server.mjs',
       startServerReadyPattern: 'LHCI server ready',
       startServerReadyTimeout: 60000,
-      numberOfRuns: 1,
+      numberOfRuns: 3,
       url: [
         `${LHCI_ORIGIN}/ru/how-this-site-is-built`,
         `${LHCI_ORIGIN}/ru/articles`,
@@ -22,15 +22,33 @@ module.exports = {
     assert: {
       preset: 'lighthouse:recommended',
       assertions: {
-        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:performance': [
+          'error',
+          { minScore: 0.9, aggregationMethod: 'median' },
+        ],
         'categories:accessibility': ['error', { minScore: 0.9 }],
         'categories:best-practices': ['error', { minScore: 0.9 }],
         'categories:seo': ['error', { minScore: 0.9 }],
-        'first-contentful-paint': ['error', { maxNumericValue: 1800 }],
-        'largest-contentful-paint': ['error', { maxNumericValue: 2500 }],
-        'speed-index': ['error', { maxNumericValue: 3000 }],
-        'total-blocking-time': ['error', { maxNumericValue: 200 }],
-        'cumulative-layout-shift': ['error', { maxNumericValue: 0.1 }],
+        'first-contentful-paint': [
+          'error',
+          { maxNumericValue: 1800, aggregationMethod: 'median' },
+        ],
+        'largest-contentful-paint': [
+          'error',
+          { maxNumericValue: 2500, aggregationMethod: 'median' },
+        ],
+        'speed-index': [
+          'error',
+          { maxNumericValue: 3000, aggregationMethod: 'median' },
+        ],
+        'total-blocking-time': [
+          'error',
+          { maxNumericValue: 200, aggregationMethod: 'median' },
+        ],
+        'cumulative-layout-shift': [
+          'error',
+          { maxNumericValue: 0.1, aggregationMethod: 'median' },
+        ],
         'performance-budget': 'error',
         'document-latency-insight': ['warn', {}],
         'forced-reflow-insight': ['warn', {}],
